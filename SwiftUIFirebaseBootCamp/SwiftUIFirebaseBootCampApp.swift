80807d2cde3fbb8b849a9bf39179048351daabdc
jacobkosmart <jacobkosmart@gmail.com>
2023-03-28 11:35:04 +1300

    Initial Commit

---
+++ SwiftUIFirebaseBootCamp/SwiftUIFirebaseBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseBootCampApp.swift
+//  SwiftUIFirebaseBootCamp
+//
+//  Created by Jacob Ko on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
